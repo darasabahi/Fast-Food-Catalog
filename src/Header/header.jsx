94357833cd5cdb2ee4423f772133015f0ce46330
darasabahi <dara.sabahi@yahoo.com>
2023-09-08 15:53:09 +0330

    add cart icon

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,13 +1,18 @@
-import "./header.css" 
-const Header=()=>{
-    return(
-    <header className="bg-size-caver bg-position-center py-5"> 
-    <div className="container">
+import "./header.css";
+import { HiShoppingCart } from "react-icons/hi";
+const Header = () => {
+  return (
+    <header className="bg-size-caver bg-position-center py-5">
+      <div className="container">
         <h2 className="text-start text-light text-uppercase">
-            fast food catolog
+          fast food catolog
         </h2>
-    </div>
+        <div className="cart-icon">
+          <HiShoppingCart className="fs-3 ms-1 mt-1"></HiShoppingCart>
+          <div className="total-cart-icon  ">0</div>
+        </div>
+      </div>
     </header>
-    )
-}
-export default Header
+  );
+};
+export default Header;
